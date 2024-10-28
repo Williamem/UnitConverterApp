4e210fb3ba51cb5f3ef53901c5dd42a7f1e48e01
WilliamEm <william.emanuelsson@gmail.com>
2024-10-28 11:24:58 +0100

    implement Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View, TextInput, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type HeaderProps = {
@@ -20,12 +20,13 @@ const Header: React.FC<HeaderProps> = ({
   const router = useRouter();
 
   return (
+    <SafeAreaView edges={["top"]} className="bg-background-primary">
       <View className="flex-row items-center bg-background-tertiary p-2">
         {showBackButton && (
           <TouchableOpacity onPress={() => router.back()} className="mr-2">
-            <View>
+            <Text>
               <Ionicons name="arrow-back" size={24} color="#475569" />
-            </View>
+            </Text>
           </TouchableOpacity>
         )}
         <TextInput
@@ -34,12 +35,16 @@ const Header: React.FC<HeaderProps> = ({
           placeholderTextColor="#94a3b8"
           onChangeText={(text) => onSearch(text)}
         />
-        <TouchableOpacity onPress={() => router.push('/settings')} className="ml-2">
-          <View>
+        <TouchableOpacity
+          onPress={() => router.push("/settings")}
+          className="ml-2"
+        >
+          <Text>
             <Ionicons name="settings-outline" size={24} color="#475569" />
-          </View>
+          </Text>
         </TouchableOpacity>
       </View>
+    </SafeAreaView>
   );
 };
 

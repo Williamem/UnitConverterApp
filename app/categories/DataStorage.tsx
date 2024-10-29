9d7ab8a147da030e5cc70d425cfeee67ffcbb687
WilliamEm <william.emanuelsson@gmail.com>
2024-10-29 10:52:16 +0100

    delete no longer needed files

--- app/categories/DataStorage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import UnitConverter from '../components/unit-converter/UnitConverter';
-
-const DataStorage: React.FC = () => {
-  return <UnitConverter unitCategory="dataStorage" />;
-};
-
-export default DataStorage;
-
